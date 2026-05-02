687f1bd42152314ecb65307811f3d90dda86d864
Mohamed Ateto <mohamedhosny6757@gmail.com>
2026-05-02 15:10:37 +0300

    feat: AURA E-Commerce ASP.NET Core API

---
+++ src/ECommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,44 @@
+using ECommerce.Application.Interfaces;
+using ECommerce.Infrastructure.Persistence;
+using ECommerce.Infrastructure.Repositories;
+using ECommerce.Infrastructure.Security;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(configuration.GetSection(JwtOptions.SectionName));
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        var usePostgres = Environment.GetEnvironmentVariable("USE_POSTGRES") == "true"
+                       || connectionString.StartsWith("Host=")
+                       || connectionString.StartsWith("postgres");
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            if (usePostgres)
+                options.UseNpgsql(connectionString);
+            else
+                options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ICartRepository, CartRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IReviewRepository, ReviewRepository>();
+        services.AddScoped<IDiscountRepository, DiscountRepository>();
+        services.AddScoped<INewsletterRepository, NewsletterRepository>();
+        services.AddScoped<IPasswordHasher, BcryptPasswordHasher>();
+        services.AddScoped<ITokenService, TokenService>();
+
+        return services;
+    }
+}
